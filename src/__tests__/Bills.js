fd1c185bc17b7619da3a6474ac3e1397e4fcffbe
Kevin Mazats <k.mazats@gmail.com>
2021-05-24 16:00:43 +0200

    // WIP container/bills methods
    weird results

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,10 +1,32 @@
 import { screen } from "@testing-library/dom";
-import BillsUI from "../views/BillsUI.js";
+import userEvent from "@testing-library/user-event";
+
+import { ROUTES } from "../constants/routes";
+
+import { localStorageMock } from "../__mocks__/localStorage.js";
+
 import { bills } from "../fixtures/bills.js";
+
+import firestore from "../app/Firestore.js";
+
+import BillsUI from "../views/BillsUI.js";
 import ErrorPage from "../views/ErrorPage.js";
 import LoadingPage from "../views/LoadingPage.js";
 
+import Bills from "../containers/Bills.js";
+
 describe("Given I am connected as an employee", () => {
+	beforeEach(() => {
+		Object.defineProperty(window, "localStorage", {
+			value: localStorageMock,
+		});
+		window.localStorage.setItem(
+			"user",
+			JSON.stringify({
+				type: "Employee",
+			})
+		);
+	});
 	describe("When I am on Bills Page", () => {
 		test("Then bill icon in vertical layout should be highlighted", () => {
 			const html = BillsUI({ data: [] });
@@ -36,6 +58,106 @@ describe("Given I am connected as an employee", () => {
 			const billsHtml = BillsUI({ data: [], loading: false, error: "Whoops!" });
 			const errorHtml = ErrorPage("Whoops!");
 			expect(billsHtml.indexOf(errorHtml) > -1).toBeTruthy();
+		});
+	});
+	// describe("When I click on new bill button", () => {
+	// 	test("Then I should go to the new bill form page", async () => {
+	// 		const html = BillsUI({ data: bills });
+	// 		document.body.innerHTML = html;
+	// 		const onNavigate = (pathname) => {
+	// 			document.body.innerHTML = ROUTES({ pathname });
+	// 		};
+	// 		const container = new Bills({
+	// 			document,
+	// 			onNavigate,
+	// 			firestore,
+	// 			localStorage: window.localStorage,
+	// 		});
+
+	// 		const handleClickNewBill = jest.fn(container.handleClickNewBill);
+	// 		const button = screen.getByTestId("btn-new-bill");
+	// 		document.addEventListener("DOMContentLoaded", function () {
+	// 			button.addEventListener("click", handleClickNewBill);
+	// 		});
+
+	// 		userEvent.click(button);
+	// 		expect(handleClickNewBill).toHaveBeenCalled();
+	// 		expect(screen.getByTestId("form-new-bill") !== undefined).toBeTruthy();
+	// 	});
+	// });
+	describe("When I click on eye icon", () => {
+		let container,eye,handleClickIconEye;
+		beforeAll(() => {
+			const html = BillsUI({ data: bills });
+			document.body.innerHTML = html;
+			const onNavigate = (pathname) => {
+				document.body.innerHTML = ROUTES({ pathname });
+			};
+			container = new Bills({
+				document,
+				onNavigate,
+				firestore,
+				localStorage: window.localStorage,
+			});
+			eye = screen.getAllByTestId("icon-eye")[0];
+			handleClickIconEye = jest.fn(container.handleClickIconEye);
+			document.addEventListener("DOMContentLoaded", function () {
+				eye.addEventListener("click", handleClickIconEye);
+			});
+			
+			
+		});
+		test("Then a modal should open", () => {
+			// const eye = screen.getAllByTestId("icon-eye")[0];
+			// const handleClickIconEye = jest.fn(container.handleClickIconEye);
+			userEvent.click(eye);
+			expect(handleClickIconEye).toHaveBeenCalled();
+			const modale = screen.getByTestId("modaleFile");
+			expect(modale).toBeTruthy();
+			//
+			// const html = BillsUI({ data: bills });
+			// document.body.innerHTML = html;
+			// const onNavigate = (pathname) => {
+			// 	document.body.innerHTML = ROUTES({ pathname });
+			// };
+			// const container = new Bills({
+			// 	document,
+			// 	onNavigate,
+			// 	firestore,
+			// 	localStorage: window.localStorage,
+			// });
+			// const handleClickIconEye = jest.fn(container.handleClickIconEye);
+			// const eye = screen.getAllByTestId("icon-eye")[0];
+			//
+			// This one pass the test but gives no coverage
+			//
+			// document.addEventListener("DOMContentLoaded", function () {
+			// eye.addEventListener("click", handleClickIconEye);
+			// userEvent.click(eye);
+			// expect(handleClickIconEye).toHaveBeenCalled();
+			// const modale = screen.getByTestId("modaleFile");
+			// expect(modale).toBeTruthy();
+			// })
+			//
+			// This one fail the test but gives coverage
+			//
+			// document.addEventListener("DOMContentLoaded", function () {
+			// eye.addEventListener("click", handleClickIconEye);
+			// });
+			// userEvent.click(eye);
+			// expect(handleClickIconEye).toHaveBeenCalled();
+			// const modale = screen.getByTestId("modaleFile");
+			// expect(modale).toBeTruthy();
+			//
+			// This one add coverage but fail the test and throw 2 errors :
+			// 		TypeError: $(...).modal is not a function
+			//		TypeError: icon.getAttribute is not a function
+			//
+			// eye.addEventListener("click", handleClickIconEye);
+			// userEvent.click(eye);
+			// expect(handleClickIconEye).toHaveBeenCalled();
+			// const modale = screen.getByTestId("modaleFile");
+			// expect(modale).toBeTruthy();
 		});
 	});
 });
